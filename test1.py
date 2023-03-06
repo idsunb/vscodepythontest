0c2874236f5c5cd2c3e820b559d2192e411ddc78
idsunb <idsunb@gmail.com>
2023-03-06 23:39:47 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,16 @@
+import numpy
+#print some worx
+def printHelloWorld() -> None: 
+    print("Hello World") 
+    
+def main() -> None: 
+    printHelloWorld()
+    printHelloWorld() 
+    printHelloWorld() 
+    printHelloWorld() 
+    printHelloWorld() 
+    
+if __name__ == "__main__": 
+    main() 
+
+#创建github仓库
